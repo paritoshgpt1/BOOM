42a5bfcf511b51ad199000ad1b12cc77d9f469f5
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2019-05-06 22:11:23 -0400

    Documented methods in run.py as per doxygen

--- bin/run.py
+++ bin/run.py
@@ -3,34 +3,43 @@ import os
 import subprocess
 
 
-def file_filter(filename, radical='', extension=''):
-    """Check if a filename matches a radical and extension"""
+## Check if a filename matches the startswith string and extension"
+#  @param filename The name of the file
+#  @param startswith The string with which the file should start
+#  @param extension The extension of the files that we want to filter
+#  @return boolean indicating if it is a match or not
+def file_filter(filename, startswith='', extension=''):
+    """Check if a filename matches the startswith string and extension"""
     if not filename:
         return False
     filename = filename.strip()
-    return filename.startswith(radical) and filename.endswith(extension)
+    return filename.startswith(startswith) and filename.endswith(extension)
 
 
-def dir_filter(dirname='', radical='', extension=''):
-    """Filter filenames in directory according to radical and extension"""
+## Filter filenames in directory according to radical and extension
+#  @param dirname The directory in which we want to search for the files
+#  @param startswith The string with which the file should start
+#  @param extension The extension of the files that we want to filter
+#  @return list of files matching our filters
+def dir_filter(dirname='', startswith='', extension=''):
     if not dirname:
         dirname = '.'
     return [filename for filename in os.listdir(dirname)
-            if file_filter(filename, radical, extension)]
+            if file_filter(filename, startswith, extension)]
 
 
-# The function to execute a command in a subprocess
+## The function to execute a command in a subprocess
 #  @param cmd The command to be executed
-#  @return None if in debug mode, the subprocess instance otherwise
+#  @return the subprocess instance
 def execute_cmd(cmd):
     log.debug('Execute ' + ' '.join(cmd))
     return subprocess.Popen(cmd)
 
 
-# Directory containing all the zipped modules
+## Directory containing all the zipped modules
 dirname = './tmp'
 filenames = dir_filter(dirname=dirname, extension='.zip')
-print(filenames)
+log.debug(filenames)
 
 process_list = []
 
@@ -39,6 +48,6 @@ for filename in filenames:
     log.debug(cmd)
     process_list.append(execute_cmd(cmd))
 
-# Wait for processes to finish.
+## Wait for processes to finish.
 for process in process_list:
     process.wait()
