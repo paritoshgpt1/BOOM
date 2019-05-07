a3ba08f9aa8db452d8f90ededccb6280e4dbfc34
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2019-05-06 22:56:21 -0400

    Add instructions to create your own ami

--- examples/toy/infra/initialize.sh
+++ examples/toy/infra/initialize.sh
@@ -7,8 +7,33 @@
 # back to the S3 bucket
 
 #### Install basic software ####
+#### These steps are not required if you are using the custom ami ####
+# Please enable these commands if you want to use a base ubuntu image (Ubuntu 16.04)
 # sudo apt update -y
 # sudo apt install awscli -y
+# sudo apt install make -y
+# sudo apt-get install python-pip -y
+# # Install RabbitMQ (https://www.rabbitmq.com/install-debian.html#apt)
+# wget -O - "https://packagecloud.io/rabbitmq/rabbitmq-server/gpgkey" | sudo apt-key add -
+# sudo apt-key adv --keyserver "hkps.pool.sks-keyservers.net" --recv-keys "0x6B73A36E6026DFCA"
+# wget -O - "https://github.com/rabbitmq/signing-keys/releases/download/2.0/rabbitmq-release-signing-key.asc" | sudo apt-key add -
+# sudo apt-get install apt-transport-https -y
+# echo "deb https://dl.bintray.com/rabbitmq/debian xenial main" | sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list
+# sudo tee /etc/apt/sources.list.d/bintray.rabbitmq.list <<EOF
+# deb https://dl.bintray.com/rabbitmq-erlang/debian xenial erlang
+# deb https://dl.bintray.com/rabbitmq/debian xenial main
+# EOF
+# sudo apt-get update -y
+# sudo apt-get install -y rabbitmq-server
+# sudo apt install virtualenv -y
+# cd /home/ubuntu
+# virtualenv env
+# source env/bin/activate
+# git clone https://github.com/paritoshgpt1/BOOM.git
+# cd /home/ubuntu/BOOM
+# pip install -r requirements.txt
+# cd /home/ubuntu
+
 
 #### Start RabbitMQ Server ####
 sudo service rabbitmq-server start
